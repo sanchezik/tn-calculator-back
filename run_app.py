c1de3dc4ea1900419d85cd54129159f73ca2e02f
Alexander Tsetsersky <sanchezlife@tut.by>
2024-07-21 04:27:34 -0300

    change Cookie Secure param

--- run_app.py
+++ run_app.py
@@ -14,7 +14,7 @@ app.secret_key = config.APP_SESSION_SECRET
 app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_COOKIE_SAMESITE'] = 'None'
-app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_SECURE'] = False
 Session(app)
 CORS(app)
 
