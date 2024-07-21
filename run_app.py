3866ead21ef092a407089e0b7f794e46006874c1
Alexander Tsetsersky <sanchezlife@tut.by>
2024-07-21 04:00:18 -0300

    set Cookie samesite to None

--- run_app.py
+++ run_app.py
@@ -13,6 +13,7 @@ app.secret_key = config.APP_SESSION_SECRET
 
 app.config["SESSION_PERMANENT"] = False
 app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
 Session(app)
 CORS(app)
 
